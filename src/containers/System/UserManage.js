e844c9fdc1d4abe5dfc71e4dedcdc64d5d587955
Nguyen Huy <huyit1999a@gmail.com>
2022-01-07 11:24:25 +0700

    create, delete user

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,150 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import {
+	getAllUsers,
+	createNewUserService,
+	deleteUserService,
+} from '../../services/userService';
+import { emitter } from '../../utils/emitter';
+import ModalUser from './ModalUser';
+
 class UserManage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			arrUsers: [],
+			isOpenModalUser: false,
+		};
+	}
 
-    state = {
+	async componentDidMount() {
+		await this.getAllUsers();
+	}
 
-    }
+	handleAddNewUser = () => {
+		this.setState({
+			isOpenModalUser: true,
+		});
+	};
 
-    componentDidMount() {
+	getAllUsers = async () => {
+		let response = await getAllUsers('ALL');
+		if (response && response.errCode === 0) {
+			this.setState({
+				arrUsers: response.users,
+			});
+		}
+	};
 
-    }
+	toggleUserModal = () => {
+		this.setState({
+			isOpenModalUser: !this.state.isOpenModalUser,
+		});
+	};
 
+	createNewUser = async (data) => {
+		try {
+			let response = await createNewUserService(data);
+			if (response && response.errCode !== 0) {
+				alert(response.errMessage);
+			} else {
+				await this.getAllUsers();
+				this.setState({
+					isOpenModalUser: false,
+				});
 
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
+				emitter.emit('EVENT_CLEAR_MODAL_DATA');
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
+	handleDeleteUser = async (user) => {
+		try {
+			let response = await deleteUserService(user.id);
+			if (response && response.errCode === 0) {
+				await this.getAllUsers();
+			} else {
+				alert(response.errMessage);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	render() {
+		let arrUsers = this.state.arrUsers;
+		return (
+			<div className="users-container">
+				<ModalUser
+					isOpen={this.state.isOpenModalUser}
+					toggleFromParent={this.toggleUserModal}
+					createNewUser={this.createNewUser}
+				/>
+				<div className="title text-center">Manage users</div>
+				<div className="mx-1">
+					<button
+						className="btn btn-primary px-3"
+						onClick={() => this.handleAddNewUser()}
+					>
+						<i className="fas fa-plus"></i> Add new user
+					</button>
+				</div>
+				<div className="users-table mt-3 mx-1">
+					<table id="users">
+						<thead>
+							<tr>
+								<th>Email</th>
+								<th>First Name</th>
+								<th>Last Name</th>
+								<th>Address</th>
+								<th>Actions</th>
+							</tr>
+						</thead>
+						<tbody>
+							{arrUsers &&
+								arrUsers.map((item, index) => {
+									return (
+										<tr key={index}>
+											<td>{item.email}</td>
+											<td>{item.firstName}</td>
+											<td>{item.lastName}</td>
+											<td>{item.address}</td>
+											<td>
+												<button className="btn-edit">
+													<i className="fas fa-pencil-alt"></i>
+												</button>
+												<button
+													className="btn-delete"
+													onClick={() => {
+														this.handleDeleteUser(
+															item
+														);
+													}}
+												>
+													<i className="fas fa-trash"></i>
+												</button>
+											</td>
+										</tr>
+									);
+								})}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+	return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+	return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -7,4 +7,16 @@ const handleLoginApi = (userEmail, userPassword) => {
 	});
 };
 
-export { handleLoginApi };
+const getAllUsers = (inputId) => {
+	return axios.get(`/api/get-all-users?id=${inputId}`);
+};
+
+const createNewUserService = (data) => {
+	return axios.post('/api/create-new-user', data);
+};
+
+const deleteUserService = (userId) => {
+	return axios.delete('/api/delete-user', { data: { id: userId } });
+};
+
+export { handleLoginApi, getAllUsers, createNewUserService, deleteUserService };
